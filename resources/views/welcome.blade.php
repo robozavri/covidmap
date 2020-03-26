bdb4e1489c9949dfe55350304fdce788a2b641a1
robozavri <nika37912@mail.ru>
2020-03-26 14:13:08 +0400

    rm x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -187,9 +187,7 @@
             <div class="modal-content">
                 <div class="modal-header">
 {{--                    <h5 class="modal-title">Modal title</h5>--}}
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+
                 </div>
                 <div class="modal-body">
                     <p>საიტი მუშაობს საჩვენებელ რეჟიმში და განკუთვნილია მხოლოდ პრეზენტაციისთვის.
